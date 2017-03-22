097946879360e9c4f3cc37a896d539d20fa92b1d
zak1019010 <zak1019010@yahoo.com>
2017-03-22 16:34:01 -0400

    updated project

---
+++ AndroidStudioProjects/Anagram/app/src/main/java/anagrams_starter/app/src/test/java/com/google/engedu/anagrams/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package anagrams_starter.app.src.test.java.com.google.engedu.anagrams;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
